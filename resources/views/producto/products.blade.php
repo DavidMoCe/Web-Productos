2f0adb3b2cd8df9a13856054de7f9384233e42d9
David Moreno Cerezo <131790198+DavidMoCe@users.noreply.github.com>
2024-11-13 15:09:51 +0100

    Update products.blade.php

--- resources/views/producto/products.blade.php
+++ resources/views/producto/products.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <!-- <title>Shop Homepage - Start Bootstrap Template</title> -->
-        <title>TouchPhone</title>
+        <title>Titulo proyecto</title>
         <!-- Favicon-->
         <!-- <link rel="icon" type="image/x-icon" href="assets/favicon.ico" /> -->
 
